0cb18f6f6617d40d59e47250d2b777a7b672cd8a
Christian Ye <ye56119537@gmail.com>
2019-09-18 19:10:15 +0800

    init test

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name='scrapyd_egg_checksum',
+     version='0.1',
+     author="Xiaodong Ye",
+     author_email="470504024@qq.com",
+     description="Get the checksum of eggs in case of building distributed scrapy clusters",
+     packages=["scrapyd_egg_checksum"],
+     license='MIT'
+ )
