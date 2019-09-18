5ca9c9d4f66bff44c4e9d18f695750e3f4b16f9d
Christian Ye <ye56119537@gmail.com>
2019-09-18 19:40:26 +0800

    requirements update

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
 setuptools.setup(
-     name='scrapyd_egg_checksum',
-     version='0.1',
-     author="Xiaodong Ye",
-     author_email="470504024@qq.com",
-     description="Get the checksum of eggs in case of building distributed scrapy clusters",
-     packages=["scrapyd_egg_checksum"],
-     license='MIT'
- )
+    name='scrapyd_egg_checksum',
+    version='0.1',
+    author="Xiaodong Ye",
+    author_email="470504024@qq.com",
+    url='https://github.com/ChristianYeah/scrapyd-egg-checksum',
+    description="Get the checksum of eggs in case of building distributed scrapy clusters",
+    packages=["scrapyd_egg_checksum"],
+    install_requires=[
+        'scrapy',
+        'scrapyd',
+    ],
+    license='MIT'
+)
